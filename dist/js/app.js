06872e9b896b70d2a9d1212b60dce2bfbffa135b
paoloferrari97 <ferrari.paolo.97@gmail.com>
2021-06-15 11:19:38 +0200

    Creo e completo navbar (useo vuejs)

--- dist/js/app.js
+++ dist/js/app.js
@@ -7,7 +7,15 @@
   \********************/
 /***/ (() => {
 
-
+var app = new Vue({
+  el: "#app",
+  data: {
+    linkNavbar: ["Home", "Pages", "Course Formats", "Courses", "Demos"],
+    socials: ["facebook-square", "twitter", "instagram", "linkedin", "google-plus", "youtube"]
+  },
+  methods: {},
+  mounted: function mounted() {}
+});
 
 /***/ }),
 
---
+++ src/app.js
@@ -0,0 +1,13 @@
+const app = new Vue({
+    el: "#app",
+    data: {
+        linkNavbar: ["Home", "Pages", "Course Formats", "Courses", "Demos"],
+        socials:["facebook-square", "twitter", "instagram", "linkedin", "google-plus", "youtube"]
+    },
+    methods: {
+        
+    },
+    mounted() {
+        
+    }
+});
