6a487111045e673f4b7802a76319a8b542e0a163
paoloferrari97 <ferrari.paolo.97@gmail.com>
2021-06-15 14:09:47 +0200

    Termino ultima parte dell'header

--- dist/js/app.js
+++ dist/js/app.js
@@ -11,7 +11,23 @@ var app = new Vue({
   el: "#app",
   data: {
     linkNavbar: ["Home", "Pages", "Course Formats", "Courses", "Demos"],
-    socials: ["facebook-square", "twitter", "instagram", "linkedin", "google-plus", "youtube"]
+    socials: ["facebook-square", "twitter", "instagram", "linkedin", "google-plus", "youtube"],
+    activities: [{
+      name: "Business",
+      icon: "fas fa-chart-line"
+    }, {
+      name: "Design",
+      icon: "fas fa-palette"
+    }, {
+      name: "Development",
+      icon: "fas fa-cog"
+    }, {
+      name: "Lifestyle",
+      icon: "far fa-smile"
+    }, {
+      name: "Office Productivity",
+      icon: "fas fa-print"
+    }]
   },
   methods: {},
   mounted: function mounted() {}
