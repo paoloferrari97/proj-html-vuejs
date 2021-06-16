775e1214ca075f7ceb3d470172b13b8969a0f46c
paoloferrari97 <ferrari.paolo.97@gmail.com>
2021-06-16 16:22:32 +0200

    Sistemo sezione 2 del main

--- dist/js/app.js
+++ dist/js/app.js
@@ -28,49 +28,6 @@ var app = new Vue({
       name: "Office Productivity",
       icon: "fas fa-print"
     }],
-    cards: [{
-      img: "895786_7b4b_2-272x161.jpg",
-      category: "Development",
-      title: "The Complete iOS 10 & Swift 3 Developer Course",
-      vote: 3,
-      fullPrice: "199.99",
-      price: "100"
-    }, {
-      img: "246154_d8b0_3-272x161.jpg",
-      category: "Development",
-      title: "Web Design for Beginners: Real World Coding in HTML & CSS",
-      vote: 4,
-      fullPrice: "129.99",
-      price: "65"
-    }, {
-      img: "752950_b773-272x161.jpg",
-      category: "Development",
-      title: "GitHub Ultimate: Master Git and GitHub - Beginner to Expert",
-      vote: 4,
-      fullPrice: "99.99",
-      price: "50"
-    }, {
-      img: "1253188_58f7_2-272x161.jpg",
-      category: "Development",
-      title: "The Complete iOS 11 & Swift Developer Course - Build 20 Apps",
-      vote: 3,
-      fullPrice: "199.99",
-      price: "100"
-    }, {
-      img: "919872_ed54_6-272x161.jpg",
-      category: "Development",
-      title: "Android Java Masterclass - Become an App Developer",
-      vote: 5,
-      fullPrice: "99.99",
-      price: "50"
-    }, {
-      img: "951684_9c1a_2-272x161.jpg",
-      category: "Development",
-      title: "Xamarin Forms: Build Native Cross-platform Apps with C#",
-      vote: 4,
-      fullPrice: "189.99",
-      price: "95"
-    }],
     categories: ["All Categories", "Business", "Design", "Development", "IT & Software", "Lifestyle", "Marketing", "Office Productivity"],
     category_selected: "All Categories",
     cardsSec4: [{
